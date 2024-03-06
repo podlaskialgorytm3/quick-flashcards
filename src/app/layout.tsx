b0b59e66ca892d7dba3cf7851efd39e3b73e3263
podlaskialgorytm3 <live.m851@gmail.com>
2024-03-06 22:32:30 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Quick Flashcards",
+    default: "Quick Flashcards"
+  },
+  description: "A simple app, which helps you to learn faster and more effectively.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
