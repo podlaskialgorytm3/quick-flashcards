511f661dec93c7e4db767db0255f8a8ad166d92e
podlaskialgorytm3 <live.m851@gmail.com>
2024-03-13 23:49:19 +0100

    feat: create two page: one with login page, second with register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from '@mui/material';
+import Link from 'next/link';
 
 const Home = () => {
   return (
     <div className="flex justify-center items-center h-[100vh] flex-col">
+      <Button 
+          variant="text" 
+          color="inherit" 
+          style={{marginTop: '20px',fontSize: '20px',position: 'absolute', top: '2px', right: '100px'}}>
+            <Link href='/login'>Log In</Link>
+      </Button>
       <h1 className="text-[72px]">Quick Flashcards</h1>
-      <Button variant="outlined" color="inherit" style={{marginTop: '20px',fontSize: '20px'}}>Get Started</Button>
+      <Button variant="outlined" color="inherit" style={{marginTop: '20px',fontSize: '20px'}}><Link href='/register'>Get Started</Link></Button>
     </div>
   );
 }
---
+++ src/app/register/page.tsx
@@ -0,0 +1,10 @@
+const Register = () => {
+    return (
+        <div>
+            <h1>Get Started</h1>
+            <p>Get started with React</p>
+        </div>
+    )
+}
+
+export default Register
